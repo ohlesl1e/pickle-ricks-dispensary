b90c84260aaf8e6629d0069a5f555f72f7e3aeee
dpolozov <dpolozov@gmail.com>
2020-05-05 15:14:37 -0700

    inventory server pulls inventory info from mongo db
    check to make sure it only doing it once. right now it does it in the home page component

--- frontend/src/redux/actions/inventoryAction.js
+++ frontend/src/redux/actions/inventoryAction.js
@@ -3,6 +3,23 @@ export const setItem = item => ({
     item
 })
 
+export const setInventory = inventory => ({
+    type: 'INVENTORY_SET_INVENTORY',
+    inventory
+})
+
 export const selectItem = index => (dispatch, getState) => {
     dispatch(setItem(getState().inventoryReducer.inventory[index]))
 }
+
+export const getInventory = () => (dispatch) => {
+    const url = '/api/inventory/get';
+
+    fetch(url)
+        .then( res => res.json())
+        .then( data => {
+            dispatch(setInventory(data));
+        })
+        .catch(console.log);
+
+};
