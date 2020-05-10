1152a74661c47a4d4f4219a29ac8a34b8bc57beb
Leslie Zhou <zzhou2@mail.sfsu.edu>
2020-05-10 12:39:29 -0700

    email service

---
+++ backend/mailService.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer')
+const express = require('express')
+const app = express()
+const port = 3006;
+app.use(express.json())
+
+app.post('/mail/sendemail', (req, res) => {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: '',
+            pass: '',
+        }
+    })
+
+    let mailOptions = {
+        from: {
+            name: 'Pickle Rick',
+            address: '',
+        },
+        to: req.body.email,
+        replyTo: '',
+        subject: 'Thank You for Your Order',
+        text: req.body.message,
+        html: `<p>${req.body.message}</p>`,
+    }
+    
+    transporter.sendMail(mailOptions, (err, info) =>{
+		if (err) {
+			res.send(err)
+		}
+		res.send(info)
+	})
+})
+
+app.listen(port, () => console.log(`email service listening on port ${port}!`))
