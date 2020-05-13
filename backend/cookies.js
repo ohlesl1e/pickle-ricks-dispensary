61d2230a106658e8fd8dee3ada5202a7d849d48d
HW 1.3 <cvelasc4@mail.sfsu.edu>
2020-05-12 22:43:34 -0700

    New changes to cookies

--- backend/cookies.js
+++ backend/cookies.js
@@ -5,67 +5,8 @@ const app = express();
 const client = redis.createClient();
 const port = 3004;
 
-app.use(express.json());
+document.cookie = "key=value";
 
-const url = "mongodb://localhost:27017";
-
-const dbName = "MyDatabase";
-
-const dbClient = new MongoClient(url);
-
-var Cookie = require("tiny-cookie");
-
-var redisClient = redis.createClient(6379, "127.0.0.1");
-var RedisStore = require("connect-redis")(session);
-var session = require("express-session");
-
-app.use(
-  session({
-    store: new RedisStore({ client: redisClient }),
-    secret: "gBpwmwE0PmyDKPuLhhmY8CONJQW3TnCujQuoE8nVao",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true },
-  })
-);
-
-redisClient.on("error", function (err) {
-  console.log(" Error " + err);
-});
-
-var VueCookie = {
-  install: function (Vue) {
-    Vue.prototype.$cookie = this;
-    Vue.cookie = this;
-  },
-  set: function (name, value, daysOrOptions) {
-    var opts = daysOrOptions;
-    if (Number.isInteger(daysOrOptions)) {
-      opts = { expires: daysOrOptions };
-    }
-    return Cookie.set(name, value, opts);
-  },
-
-  get: function (name) {
-    return Cookie.get(name);
-  },
-
-  delete: function (name, options) {
-    var opts = { expires: -1 };
-    if (options !== undefined) {
-      opts = Object.assign(options, opts);
-    }
-    this.set(name, "", opts);
-  },
-};
-
-if (typeof exports == "object") {
-  module.exports = VueCookie;
-} else if (typeof define == "function" && define.amd) {
-  define([], function () {
-    return VueCookie;
-  });
-} else if (window.Vue) {
-  window.VueCookie = VueCookie;
-  Vue.use(VueCookie);
+function alertCookie() {
+  alert(document.cookie);
 }
--- frontend/src/cookies.js
+++ frontend/src/cookies.js
@@ -1,49 +1,8 @@
-(function () {
-  Number.isInteger =
-    Number.isInteger ||
-    function (value) {
-      return (
-        typeof value === "number" &&
-        isFinite(value) &&
-        Math.floor(value) === value
-      );
-    };
-  var Cookie = require("tiny-cookie");
+const express = require("express");
+const redis = require("redis");
 
-  var VueCookie = {
-    install: function (Vue) {
-      Vue.prototype.$cookie = this;
-      Vue.cookie = this;
-    },
-    set: function (name, value, daysOrOptions) {
-      var opts = daysOrOptions;
-      if (Number.isInteger(daysOrOptions)) {
-        opts = { expires: daysOrOptions };
-      }
-      return Cookie.set(name, value, opts);
-    },
+const App = () => {
+  return <button onclick="alertCookie()">Show cookies</button>;
+};
 
-    get: function (name) {
-      return Cookie.get(name);
-    },
-
-    delete: function (name, options) {
-      var opts = { expires: -1 };
-      if (options !== undefined) {
-        opts = Object.assign(options, opts);
-      }
-      this.set(name, "", opts);
-    },
-  };
-
-  if (typeof exports == "object") {
-    module.exports = VueCookie;
-  } else if (typeof define == "function" && define.amd) {
-    define([], function () {
-      return VueCookie;
-    });
-  } else if (window.Vue) {
-    window.VueCookie = VueCookie;
-    Vue.use(VueCookie);
-  }
-})();
+export default App;
