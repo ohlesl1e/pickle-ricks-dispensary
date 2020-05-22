7b1037f8453fd259d6748a4c1d0dfa2fa798610e
Pramish Dhakal <pramishdhakal99@gmail.com>
2020-05-22 14:52:07 -0700

    logina nd signup fixed in homepage

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,7 +7,7 @@ import {
   setEmail,
 } from '../redux/actions/userActions';
 import { Redirect } from 'react-router-dom';
-
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 const Login = ({
   password,
@@ -27,25 +27,28 @@ const Login = ({
 
   return (
     <div>
-      <h2 className="loginTitle">Login</h2>
-      <div>
-        <label>Email:</label>
-        <input placeholder='email address'
-          value={email}
-          onChange={e => dispatch(setEmail(e.target.value))}
-        />
-      </div><br />
-      <div>
-        <label>Password:</label>
-        <input placeholder='password'
-          type="password"
-          value={password}
-          onChange={e => dispatch(setPassword(e.target.value))}
-        />
-      </div>
-      <div>
-        {loadingState === 'error' && <b>User name or password incorrect</b>}
-        <button className="login" onClick={() => dispatch(login())}>Log in</button>
+      <div className="col-xl-3 col-lg-5 col-md-8  vertical-center">
+          <Row>
+              <Form className="container login-form">
+                  <h2 className="loginTitle">Login</h2>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        <Form.Control type="text" name="email" className="login-input-email" value={email ? email : ''} placeholder="Email" onChange={e => dispatch(setEmail(e.target.value))}/>
+                      </Col>
+                  </Form.Group>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        <Form.Control type="password" name="password" className="login-input-password" value={password ? password : ''} placeholder="Password" onChange={e => dispatch(setPassword(e.target.value))}/>
+                      </Col>
+                  </Form.Group>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        {loadingState === 'error' && <b>User name or password incorrect</b>}
+                        <Button variant="primary" type="submit" className="login-button" onClick={() => dispatch(login())}>Log in</Button>
+                      </Col>
+                  </Form.Group>
+              </Form>
+          </Row>
       </div>
     </div>
   );
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -8,6 +8,7 @@ import {
   setUserType,
 } from '../redux/actions/userActions';
 import { Redirect } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 const Signup = ({
   user,
@@ -28,45 +29,50 @@ const Signup = ({
 
   return (
     <div>
-      <h2>Sign up</h2>
-      <div> 
-        {/* this is a comment */}
-        <label>Email:</label>
-        <input type="email" placeholder='Valid email address'
-          value={email}
-          onChange={e => dispatch(setEmail(e.target.value))}
-        />
-      </div><br/>
-      <div> 
-        {/* this is a comment */}
-        <label>Full Name:</label>
-        <input placeholder ='Full name'
-          value={user}
-          onChange={e => dispatch(setUser(e.target.value))}
-        />
-      </div><br/>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password" placeholder='Password'
-          value={password}
-          onChange={e => dispatch(setPassword(e.target.value))}
-        />
-      </div>
-      <div id="radioGroup" onChange={e => dispatch(setUserType(e.target.value))}>
-        <label>
-          <input type="radio" value="Buyer" name="usertype"/>
-          Buyer
-        </label>
-        <label>
-          <input type="radio" value="Seller" name="usertype"/>
-          Seller
-        </label>
-      </div>
-      <div>
-        {loadingState === 'error' && <b>User name already exists</b>}
-        {loadingState === 'Not' && <b> enter a valid email- address </b>}
-        <button id="signup" onClick={() => dispatch(create())}>Sign up</button>
+      <div className="col-xl-3 col-lg-5 col-md-8  vertical-center">
+          <Row>
+              <Form className="container login-form">
+                  <h2 className="loginTitle">Sign Up</h2>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        <Form.Control type="text" name="email" className="login-input-email" value={email ? email : ''} placeholder="Email" onChange={e => dispatch(setEmail(e.target.value))}/>
+                      </Col>
+                  </Form.Group>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        <Form.Control type="text" name="fullname" className="login-input-password" value={user ? user : ''} placeholder="Full name" onChange={e => dispatch(setUser(e.target.value))}/>
+                      </Col>
+                  </Form.Group>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        <Form.Control type="password" name="password" className="login-input-password" value={password ? password : ''} placeholder="Password" onChange={e => dispatch(setPassword(e.target.value))}/>
+                      </Col>
+                  </Form.Group>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        <Form.Check
+                          type="radio"
+                          label="Buyer"
+                          name="usertype"
+                          id="formHorizontalRadios1"
+                        />
+                        <Form.Check
+                          type="radio"
+                          label="Seller"
+                          name="usertype"
+                          id="formHorizontalRadios2"
+                        />
+                      </Col>
+                  </Form.Group>
+                  <Form.Group controlId="form">
+                      <Col className="login-form__control">
+                        {loadingState === 'error' && <b>User name already exists</b>}
+                        {loadingState === 'Not' && <b> enter a valid email- address </b>}
+                        <Button variant="primary" type="submit" className="login-button" onClick={() => dispatch(create())}>Sign up</Button>
+                      </Col>
+                  </Form.Group>
+              </Form>
+          </Row>
       </div>
     </div>
   );
