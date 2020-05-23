b4f38f3dc1efadb24000b758a4c03ed10311fe92
Leslie Zhou <zzhou2@mail.sfsu.edu>
2020-05-22 22:37:02 -0700

    add receipt to seller's sales

--- frontend/src/redux/actions/receiptActions.js
+++ frontend/src/redux/actions/receiptActions.js
@@ -1,43 +1,43 @@
 export const setReceipt = receipt => ({
-    type: 'RECEIPT_SET_RECEIPT',
-    receipt,
-  });
-  
-  export const setReceipts = receipts => ({
-    type: 'RECEIPTS_SET_RECEIPTS',
-    receipts,
-  });
+  type: 'RECEIPT_SET_RECEIPT',
+  receipt,
+});
 
-  export const displayReceipts = () => (dispatch, getState) => {
-    const userName = getState().userReducer.user;
-    const pass= getState().userReducer.password;
-    const url = '/api/receipts/get';
+export const setReceipts = receipts => ({
+  type: 'RECEIPTS_SET_RECEIPTS',
+  receipts,
+});
 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type' : 'application/json'},
-        body: JSON.stringify({ userId: userName, password:pass}),
-    }
-    fetch(url, requestOptions)
-      .then(res => res.json())
-      .then(data => {
-        dispatch(setReceipts(data));
-      })
-      .catch(console.log);
-  };
+export const displayReceipts = () => (dispatch, getState) => {
+  const userName = getState().userReducer.user;
+  const pass = getState().userReducer.password;
+  const url = '/api/receipts/get';
 
-  export const saveReceipt = () => (dispatch, getState) => {
-    const userName = getState().userReducer.user;
-    const newReceipt = getState().receiptReducer.receipt;
-    const pass= getState().userReducer.password;
-    const url = '/api/receipt/create';
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type' : 'application/json'},
-      body: JSON.stringify({ userId: userName, receipt: newReceipt, password:pass}),
-    } 
-    //console.log(requestOptions);
-    fetch(url, requestOptions)
-      .then(dispatch(setReceipt('')))
-      .catch((error) => console.log(error));
-  };
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ userId: userName, password: pass }),
+  }
+  fetch(url, requestOptions)
+    .then(res => res.json())
+    .then(data => {
+      dispatch(setReceipts(data));
+    })
+    .catch(console.log);
+};
+
+export const saveReceipt = () => (dispatch, getState) => {
+  const userName = getState().userReducer.user;
+  const newReceipt = getState().receiptReducer.receipt;
+  const pass = getState().userReducer.password;
+  const url = '/api/receipt/create';
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ userId: userName, receipt: newReceipt, password: pass }),
+  }
+  //console.log(requestOptions);
+  fetch(url, requestOptions)
+    .then(dispatch(setReceipt('')))
+    .catch((error) => console.log(error));
+};
