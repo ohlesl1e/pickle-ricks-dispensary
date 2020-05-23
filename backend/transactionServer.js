32ab813e607798511977621f4233519c5397ebf6
dibsbharati <54254648+dibsbharati@users.noreply.github.com>
2020-05-22 21:49:08 -0700

    final changes

--- backend/transactionServer.js
+++ backend/transactionServer.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { MongoClient, ObjectID } = require('mongodb');
+const KafkaProducer = require('./KafkaProducer.js');
+const producer = new KafkaProducer('email');
+const { MongoClient } = require('mongodb');
 const app = express();
 const port = 3006;
+const redis=require('redis');
+const clientr=redis.createClient();
 
 
 var bodyParser = require('body-parser');
@@ -26,7 +30,7 @@ const db=client.db(dbName);
 app.post('/api/receipts/get', (req,res) => {
       db.collection('finalUserInfo')
         .findOne({
-            userId:req.body.userId
+            email:req.body.email
         })
         .then(doc =>{
             res.send(doc.receipts);
@@ -41,52 +45,81 @@ app.post('/api/receipts/create',(req,res)=>{
     let receipt_id= req.body.receipt_id;
     let date= req.body.date;
     let totalPrice= req.body.price;
-    let items_purchased=[];
-    items_purchased.push(req.body.items);
+    let items_purchased = req.body.items;
+    //items_purchased.push(req.body.items);
     let receipt = [];
+    //console.log(req.body);
     receipt.push(receipt_id,date,totalPrice,items_purchased);
+    console.log(items_purchased);
     db.collection('finalUserInfo')
     .findOne({
-        userId:req.body.userId
+        email:req.body.email
     })
     .then(doc=>{
         if(doc.receipts){
             db.collection('finalUserInfo')
             .updateOne(
                 {
-                    userId:req.body.userId},
+                    email:req.body.email},
                 {
-                    $push: { "receipts":receipt}
+                    $push: { "receipts":receipt} 
                 }
                 )
-             .then(
-                 res.send('Receipt saved')
-            )
-            
-             .catch(e=>{
-               res.status(404).send('error404');
+             .then(()=>{
+              clientr.publish('myPubSubChannel',`${items_purchased[0]} have been purchased`)
+              console.log('Email of the receipt will be sent');
+              //console.log(receipt);
+              producer.send(req.body);
+              res.send('Receipt saved');
+             
+               
              }
-             ) 
+            )
+            .catch(e=>{
+              res.status(404).send('error404');
+            }) 
         }
         else{
              db.collection('finalUserInfo')
               .updateOne(
-                {userId:req.body.userId},
+                {email:req.body.email},
                 {
                     $set: { "receipts":receipt}
                 }
                 )
-             .then(doc =>{
-                 res.send('Receipt saved');
+             .then(() =>{
+              console.log('Email of the receipt will be sent');
+              producer.send(req.body);
+              res.send('Receipt saved');               
              })
              .catch(e=>{
                res.status(404).send('error');
-             }
-             ) 
-    }
+             }) 
+      }     
     });
+    items_purchased.forEach( (item) => {
+      console.log('in the loop');
+      console.log(item);
+      db.collection('Inventory')
+      .findOne({ title : item.title})
+      .then(doc => {
+        if(doc) { console.log(doc);}
+        
+        db.collection('Inventory')
+        .updateOne(
+          {title : item.title},
+          { $set : {stock : `${doc.stock - item.quantity}`}}
+        )
+        
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+    })
    
 });
 
-  app.listen(port, () => console.log(` Listening on port ${port}!`));
+producer.connect(() => {
+  app.listen(port);
+   });
 });
